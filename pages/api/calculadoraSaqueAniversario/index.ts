53a371dc8518c252343846588de889c343186570
JRALVES <jralves@brasilseg.com.br>
2022-08-16 19:11:39 -0300

    finish api

--- pages/api/calculadoraSaqueAniversario/index.ts
+++ pages/api/calculadoraSaqueAniversario/index.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
 import { calculoSaqueAniversario } from '../../../src/api/calculoSaqueAniversario';
 import {
   IErrorBodyData,
@@ -10,16 +11,22 @@ interface ExtendedNextApiRequest extends NextApiRequest {
   body: IRequestCalcSaque;
 }
 
-const calculadoraFgts = (
+async function calculadoraFgts(
   request: ExtendedNextApiRequest,
   response: NextApiResponse<IResponseCalcSaque | IErrorBodyData>
-) => {
+) {
+  await NextCors(request, response, {
+    methods: ['POST'],
+    origin: process.env.CORS_ORIGIN?.split(','),
+    optionsSuccessStatus: 200,
+  });
+
   if (request.method === 'POST') {
     const calculo = calculoSaqueAniversario(request.body);
     response.status(200).json(calculo);
   } else {
     response.status(405).json({ mensage: 'Method not allowed.' });
   }
-};
+}
 
 export default calculadoraFgts;
