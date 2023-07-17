48f28076a959c14cdac609cdb8e53ca333600de5
JRALVES <jralves@brasilseg.com.br>
2023-07-17 11:11:22 -0300

    refactor: adicionado object

--- pages/cardapio-x-burguer.tsx
+++ pages/cardapio-x-burguer.tsx
@@ -2,16 +2,26 @@ import type { NextPage } from 'next';
 import { Box } from '@mui/material';
 import Image from 'next/image';
 import images from '../src/assets/images';
+// import myResume from '';
 
 const Cardapio: NextPage = () => {
   const link = process.env.NEXT_PUBLIC_IMAGE_CARDAPIO;
+
   return (
     <Box>
-      <Image
+      {/* <Image
         alt="Cardápio"
         layout="fill"
         objectFit="contain"
-        src={link || images.notFound}
+        src={link ?? images.notFound}
+      /> */}
+      <object
+        data="https://www.thecampusqdl.com/uploads/files/pdf_sample_2.pdf"
+        type="application/pdf"
+        width="100%"
+        style={{
+          height: '99vh',
+        }}
       />
     </Box>
   );
